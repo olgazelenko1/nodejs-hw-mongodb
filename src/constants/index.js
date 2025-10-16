8cb485ae5149291d9a1b3d1913277b03bd8a733c
OLGA ZELENKO <olichkazelenko@gmail.com>
2025-10-16 14:07:21 +0100

    create swagger

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,20 +1,18 @@
 import path from 'node:path';
 
-
-
 export const SortOrder = {
-    ASC: 'asc',
-    DESC: 'desc',
+  ASC: 'asc',
+  DESC: 'desc',
 };
 export const FIFTEEN_MINUTES = 15 * 60 * 1000;
 export const ONE_DAY = 24 * 60 * 60 * 1000;
 
 export const SMTP = {
-    SMTP_HOST : 'SMTP_HOST',
-    SMTP_PORT : 'SMTP_PORT',
-    SMTP_USER : 'SMTP_USER',
-    SMTP_PASSWORD : 'SMTP_PASSWORD',
-    SMTP_FROM : 'SMTP_FROM',
+  SMTP_HOST: 'SMTP_HOST',
+  SMTP_PORT: 'SMTP_PORT',
+  SMTP_USER: 'SMTP_USER',
+  SMTP_PASSWORD: 'SMTP_PASSWORD',
+  SMTP_FROM: 'SMTP_FROM',
 };
 
 export const TEMPLATES_DIR = path.join(process.cwd(), 'src', 'templates');
@@ -27,3 +25,5 @@ export const CLOUDINARY = {
   API_KEY: 'CLOUDINARY_API_KEY',
   API_SECRET: 'CLOUDINARY_API_SECRET',
 };
+
+export const SWAGGER_PATH = path.join(process.cwd(), 'docs', 'swagger.json');
--- src/server.js
+++ src/server.js
@@ -7,17 +7,17 @@ import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import router from './routers/index.js';
 import cookieParser from 'cookie-parser';
 import { UPLOAD_DIR } from './constants/index.js';
-
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 const PORT = Number(getEnvVar('PORT', '3000'));
 
 export const setupServer = () => {
-
   const app = express();
 
   app.use(express.json());
   app.use(cors());
   app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
   app.use(cookieParser());
 
   app.use(
@@ -31,13 +31,12 @@ export const setupServer = () => {
   app.get('/', (req, res) => {
     res.json({
       message: 'Welcome to the Contacts API',
-      });
+    });
   });
   app.use(router);
 
   app.use(notFoundHandler);
   app.use(errorHandler);
-
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
