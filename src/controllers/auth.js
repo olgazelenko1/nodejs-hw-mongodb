c7b6bc44dd5538b9a27aaa2c843c38a4f50b5ec2
OLGA ZELENKO <olichkazelenko@gmail.com>
2025-10-16 15:54:56 +0100

    fix bag

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -3,8 +3,8 @@ import { registerUser } from '../services/auth.js';
 import { ONE_DAY } from '../constants/index.js';
 import { logoutUser } from '../services/auth.js';
 import { refreshUsersSession } from '../services/auth.js';
-import { requestResetToken }   from '../services/auth.js';
-import {resetPassword, loginOrSignupWithGoogle } from '../services/auth.js';
+import { requestResetToken } from '../services/auth.js';
+import { resetPassword, loginOrSignupWithGoogle } from '../services/auth.js';
 import { getEnvVar } from '../utils/getEnvVar.js';
 import { saveFileToCloudinary } from '../utils/saveFileToCloudinary.js';
 import { saveFileToUploadDir } from '../utils/saveFileToUploadDir.js';
@@ -12,39 +12,37 @@ import { updateContact } from '../services/contacts.js';
 import createHttpError from 'http-errors';
 import { generateAuthUrl } from '../utils/googleOAuth2.js';
 
+export const registerUserController = async (req, res) => {
+  const user = await registerUser(req.body);
 
-  export const registerUserController = async (req, res) => {
-    const user = await registerUser(req.body);
-
-    res.status(201).json({
-      status: 201,
-      massage: 'Successfully registered user!',
-      data: user,
-    });
-  };
+  res.status(201).json({
+    status: 201,
+    massage: 'Successfully registered user!',
+    data: user,
+  });
+};
 
 export const loginUserController = async (req, res) => {
-    const session = await loginUser(req.body);
+  const session = await loginUser(req.body);
 
-    res.cookie('refreshToken', session.refreshToken, {
-        httpOnly: true,
-        expires:new Date(Date.now() + ONE_DAY),
-    });
-    res.cookie('sessionId', session._id, {
-        httpOnly: true,
-        expires: new Date(Date.now() + ONE_DAY),
-    });
-    res.status(200).json({
-        status: 200,
-        message: 'Successfully logged in a user!',
-        data : {
-            accessToken: session.accessToken,
-        },
-    });
+  res.cookie('refreshToken', session.refreshToken, {
+    httpOnly: true,
+    expires: new Date(Date.now() + ONE_DAY),
+  });
+  res.cookie('sessionId', session._id, {
+    httpOnly: true,
+    expires: new Date(Date.now() + ONE_DAY),
+  });
+  res.status(200).json({
+    status: 200,
+    message: 'Successfully logged in a user!',
+    data: {
+      accessToken: session.accessToken,
+    },
+  });
 };
 
 export const logoutUserController = async (req, res) => {
-
   if (req.cookies && req.cookies.sessionId) {
     await logoutUser({ sessionId: req.cookies.sessionId });
   } else {
@@ -61,95 +59,93 @@ export const logoutUserController = async (req, res) => {
   res.clearCookie('refreshToken');
 
   res.status(204).send();
-
 };
 
-const setupSession = (res,session) => {
+const setupSession = (res, session) => {
   res.cookie('refreshToken', session.refreshToken, {
     httpOnly: true,
     expires: new Date(Date.now() + ONE_DAY),
   });
   res.cookie('sessionId', session._id, {
     httpOnly: true,
-    expires: new Date(Date.now()+ ONE_DAY),
-  });
-  };
-  
-  export const refreshUserSessionController = async (req, res) => {
-    const session = await refreshUsersSession({
-      sessionId: req.cookies.sessionId,
-      refreshToken: req.cookies.refreshToken,
-    });
-
-    setupSession(res, session);
-
-    res.json({
-      status: 200,
-      message: 'Successfully refreshed a session!',
-      data: {
-        accessToken: session.accessToken,
-      },
-    });
-  };
-
-  export const requestResetEmailController = async (req,res) => {
-    await requestResetToken(req.body.email);
-
-    res.json({
-      status: 200,
-      message: 'Reset password email was successfully sent',
-      data: {},
-    });
-  };
-
-  export const resetPwdController = async (req,res) => {
-    await resetPassword(req.body.token, req.body.password);
-
-    res.json({
-      status: 200,
-      message: 'Password has been successfully reset.',
-      data: {},
-    });
-  };
-
-  export const saveFileToCloudinaryController = async (req, res,next) => {
-
-    const {id} = req.params;
-    const photo = req.file;
-    let photoUrl;
-
-    if(photo) {
-      if(getEnvVar('ENABLE_CLOUDINARY') === 'true') {
-        photoUrl = await saveFileToCloudinary(photo);
-      } else {
-        photoUrl = await saveFileToUploadDir(photo);
-      }
-    }
-    const result = await updateContact( id , {...req.body, photo: photoUrl});
-
-    if (!result) {
-      next(createHttpError(404, 'Contact not found'));
-      return;
-    }
-    res.json({
-      status: 200,
-      message: "Successfully patched a contact!", 
-      data: result.contact,
-    });
-  };
-export const getGoogleAuthUrlController = async (req,res) => {
-  const url = generateAuthUrl();
-  res.json({
-    status:200,
-    message: 'Successfully get Google OAuth url',
-    data: {
-      url,
-    }
+    expires: new Date(Date.now() + ONE_DAY),
   });
 };
 
-export const loginWithGoogleController = async (req,res) => {
-  const session = await loginOrSignupWithGoogle(req.res.code);
+export const refreshUserSessionController = async (req, res) => {
+  const session = await refreshUsersSession({
+    sessionId: req.cookies.sessionId,
+    refreshToken: req.cookies.refreshToken,
+  });
+
+  setupSession(res, session);
+
+  res.json({
+    status: 200,
+    message: 'Successfully refreshed a session!',
+    data: {
+      accessToken: session.accessToken,
+    },
+  });
+};
+
+export const requestResetEmailController = async (req, res) => {
+  await requestResetToken(req.body.email);
+
+  res.json({
+    status: 200,
+    message: 'Reset password email was successfully sent',
+    data: {},
+  });
+};
+
+export const resetPwdController = async (req, res) => {
+  await resetPassword(req.body.token, req.body.password);
+
+  res.json({
+    status: 200,
+    message: 'Password has been successfully reset.',
+    data: {},
+  });
+};
+
+export const saveFileToCloudinaryController = async (req, res, next) => {
+  const { id } = req.params;
+  const photo = req.file;
+  let photoUrl;
+
+  if (photo) {
+    if (getEnvVar('ENABLE_CLOUDINARY') === 'true') {
+      photoUrl = await saveFileToCloudinary(photo);
+    } else {
+      photoUrl = await saveFileToUploadDir(photo);
+    }
+  }
+  const result = await updateContact(id, { ...req.body, photo: photoUrl });
+
+  if (!result) {
+    next(createHttpError(404, 'Contact not found'));
+    return;
+  }
+  res.json({
+    status: 200,
+    message: 'Successfully patched a contact!',
+    data: result.contact,
+  });
+};
+export const getGoogleAuthUrlController = async (req, res) => {
+  const url = await generateAuthUrl();
+  res.json({
+    status: 200,
+    message: 'Successfully get Google OAuth url',
+    data: {
+      url,
+    },
+  });
+};
+
+export const loginWithGoogleController = async (req, res) => {
+  const session = await loginOrSignupWithGoogle(req.body.code);
   setupSession(res, session);
 
   res.json({
@@ -160,4 +156,3 @@ export const loginWithGoogleController = async (req,res) => {
     },
   });
 };
-
